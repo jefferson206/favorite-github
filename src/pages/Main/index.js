752eef9b5b68117cc41941efd66f8e69b64d7cd7
Jefferson Mendonca <jefferson.mendonca@theos.com.br>
2024-11-30 17:08:52 -0300

    getting-repo: Axios Implementation, loading effect while is getting data,  implementation of enter in input

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,30 +1,76 @@
-import { FaGithub, FaPlus } from "react-icons/fa"; 
-import React from "react";
+import { FaGithub, FaPlus, FaSpinner } from "react-icons/fa";
+import React, { useCallback, useState } from "react";
 import { Container, Form, SubmitButton } from "./styles";
 import { useTranslation } from "react-i18next";
+import api from "services/api";
 
 function Main() {
   const { t } = useTranslation();
+  const [search, setSearch] = useState('');
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-    return (
-      <Container>
-        
-        <h1 >
-          <FaGithub size={25} />
-          { t('Main.Title') }   
-        </h1>
 
-        <Form onSubmit={() => { }}>
-          <input type="text" placeholder={t("Main.Search.Placeholder")}/>
-
-          <SubmitButton>
-            <FaPlus color="#FFF" size={14}/>
-          </SubmitButton>
-
-        </Form>
-      </Container>
-    );
+  function handleSearchChange(event) {
+    setSearch(event.target.value)
   }
-  
-  export default Main;
-  
+
+  const handleSubmit = useCallback((event) => {
+    event.preventDefault();
+    
+    async function getRepo() {
+      setLoading(true);
+      try {
+        const response = await api.get(`repos/${search}`);
+        const data = { 
+          name: response.data.full_name,  
+        };
+        
+        setRepos([...repos, data]);
+        setSearch('');
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getRepo();
+
+  }, [search, repos]);
+
+  return (
+    <Container>
+
+      <h1 >
+        <FaGithub size={25} />
+        {t('Main.Title')}
+      </h1>
+
+      <Form onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          placeholder={t("Main.Search.Placeholder")} 
+          onChange={handleSearchChange}
+          value={search}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleSubmit(e);
+            }
+          }}
+        />
+
+        <SubmitButton disabled={loading}> 
+          {loading ? (
+            <FaSpinner color="#FFF" size={14}/>
+          ) : (
+            <FaPlus color="#FFF" size={14} />
+          )}
+        </SubmitButton>
+
+      </Form>
+    </Container>
+  );
+}
+
+export default Main;
