bee7e8e0531ba8903226b785a6de9c3b6978b9dd
Jefferson Mendonca <jefferson.mendonca@theos.com.br>
2024-12-02 18:29:55 -0300

    Details: Passing params to Details repo

--- src/pages/Repos/index.js
+++ src/pages/Repos/index.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 function Repos() {
+  const { repo } = useParams();
     return (
       <div >
-        <h1>Repos</h1>
+        <h1 style={{
+          color: '#FFF'
+        }}>{decodeURIComponent(repo)}</h1>
       </div>
     );
   }
