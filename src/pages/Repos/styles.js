b87edaddab9fa109ea19987c2f3057d847e28e71
Jefferson Mendonca <jefferson.mendonca@theos.com.br>
2024-12-05 19:34:02 -0300

    Filters: Implementation of filters to display the issues, open, closed and all

--- src/pages/Repos/styles.js
+++ src/pages/Repos/styles.js
@@ -132,3 +132,20 @@ export const PageActions = styled.div`
         }
     }
 `;
+
+export const FilterList = styled.div`
+    margin: 15px 0;
+
+    button{
+        outline: 0;
+        border: 0;
+        margin: 0 3px;
+        padding: 8px;
+        border-radius: 4px;
+
+        &:nth-child(${props => props.active + 1}) {
+            background: #0071db;
+            color: #FFF;
+        }
+    }
+`;
